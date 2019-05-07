0c91b082ca7d0dc343890e8805099a86270525ba
Allan Vieira <vieira.allanc@gmail.com>
2019-05-07 00:12:32 -0300

    ajute readme; validacao cpf e cnpj

--- README.md
+++ README.md
@@ -1,25 +1,65 @@
 # Neoway Teste
 
-Microservio de importação de arquivo texto para base de dados postgresql, ecrito em GO e Docker Compose
+Microseriço de importação de arquivo texto para base de dados postgresql, ecrito em GO e Docker Compose
 
 
 
 ```
-git clone ....
+git clone https://github.com/AllanVieira/microservico-go
 ```
 
-### Installing
+### Instalando
 
 Rodando com Docker composer
-
 
 ```
 docker-compose up
 ```
 
+Preciso verificar porque não cria base de dados antes de inicar a API em GO
+Se der conexão com a base de dados recusada, roda de novo o comando :
+```
+docker-compose up
+```
+
+A aplicação vai criar automaticamente a tabela de dados:
+
+
+```
+CREATE TABLE IF NOT EXISTS CLIENTES ( 
+  id serial, 
+  cpf text, 
+  cpf_valido bool, 
+  private int, 
+  incompleto int,
+  data_ultima_compra date, 
+  ticket_medio numeric(18,2), 
+  ticket_ultima_compra numeric(18,2), 
+  loja_mais_frequente text, 
+  loja_mais_frequente_valido bool, 
+  loja_ultima_compra text, 
+  loja_ultima_compra_valido bool, 
+  created_at timestamp
+ )
+```
+
 ### Testando
 
-Acessando http://localhost vai encontrar um formalario para envio do arquivo texto
-Depois de enviado, os dados vão esta importado dentro da base de dados Files
+Acessando http://localhost vai encontrar um formulario para envio do arquivo texto
+Depois de enviado, os dados vão estar sendo importados para dentro da base de dados de nome Files
+No console da Aplicação exibir os resumos da importação, após a mensagem de fim pode conferir os registros na base de dados.
 
+Na Base de dados :
+  HOST : localhost:5432
+  USER: postgres
+  PASSWORD: postgres
+  DB: Files
+  TABLE: Clientes
+  
+Selecionar clientes com cpf inválidos :
+```
+select * from clientes where not cpf_valido
+```
 
+### TODO:
+    - Refatorar código, implementar novos pacotes para amazenar as funções do main
--- api/main.go
+++ api/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -17,21 +18,25 @@ import (
 
 //Configuração Base de Dados
 const (
-	DB_HOST     = "db" //trocar para local host quando rodar local
+	DB_HOST = "db" //trocar para local host quando rodar local
+	//DB_HOST     = "localhost"
 	DB_USER     = "postgres"
 	DB_PASSWORD = "postgres"
 	DB_NAME     = "files"
 )
 
 type Cliente struct {
-	cpf                string    `json:"cpf"`
-	private            int       `json:"private"`
-	incompleto         int       `json:"incompleto"`
-	dataUltimaCompra   time.Time `json:"data_ultima_compra"`
-	ticketMedio        float64   `json:"ticket_medio"`
-	ticketUltimaCompra float64   `json:"ticket_ultima_compra"`
-	lojaMaisFrequente  string    `json:"loja_mais_frequente"`
-	lojaUltimaCompra   string    `json:"loja_ultima_compra"`
+	cpf                     string    `json:"cpf"`
+	cpfValido               bool      `json:"cpf_valido"`
+	private                 int       `json:"private"`
+	incompleto              int       `json:"incompleto"`
+	dataUltimaCompra        time.Time `json:"data_ultima_compra"`
+	ticketMedio             float64   `json:"ticket_medio"`
+	ticketUltimaCompra      float64   `json:"ticket_ultima_compra"`
+	lojaMaisFrequente       string    `json:"loja_mais_frequente"`
+	lojaMaisFrequenteValido bool      `json:"loja_mais_frequente_valido"`
+	lojaUltimaCompra        string    `json:"loja_ultima_compra"`
+	lojaUltimaCompraValido  bool      `json:"loja_ultima_compra_valido"`
 }
 
 func main() {
@@ -44,7 +49,7 @@ func main() {
 	defer db.Close()
 
 	//Cria tabelas
-	res, err := db.Exec(" CREATE TABLE IF NOT EXISTS CLIENTES ( id serial, cpf text, private int, incompleto int, data_ultima_compra date, ticket_medio numeric(18,2), ticket_ultima_compra numeric(18,2), loja_mais_frequent text, loja_ultima_compra text, created_at timestamp)")
+	res, err := db.Exec(" CREATE TABLE IF NOT EXISTS CLIENTES ( id serial, cpf text, cpf_valido bool, private int, incompleto int, data_ultima_compra date, ticket_medio numeric(18,2), ticket_ultima_compra numeric(18,2), loja_mais_frequente text, loja_mais_frequente_valido bool, loja_ultima_compra text, loja_ultima_compra_valido bool, created_at timestamp)")
 	checkErr(err)
 	fmt.Println(res)
 
@@ -116,7 +121,12 @@ func parseFile() {
 		if string(runes[0:3]) != "CPF" {
 			cliente := Cliente{}
 
-			cliente.cpf = string(runes[0:15])
+			cliente.cpf = string(runes[0:14])
+			if ValidaCPF(cliente.cpf) != nil {
+				cliente.cpfValido = false
+			} else {
+				cliente.cpfValido = true
+			}
 
 			private, err := strconv.ParseInt(string(runes[19:20]), 10, 64)
 			checkErr(err)
@@ -146,10 +156,24 @@ func parseFile() {
 
 			if string(runes[111:115]) != "NULL" {
 				cliente.lojaMaisFrequente = string(runes[111:129])
+				if validaCNPJ(cliente.lojaMaisFrequente) != nil {
+					cliente.lojaMaisFrequenteValido = false
+				} else {
+					cliente.lojaMaisFrequenteValido = true
+				}
+			} else {
+				cliente.lojaMaisFrequenteValido = false
 			}
 
 			if string(runes[131:135]) != "NULL" {
 				cliente.lojaUltimaCompra = string(runes[131:149])
+				if validaCNPJ(cliente.lojaUltimaCompra) != nil {
+					cliente.lojaUltimaCompraValido = false
+				} else {
+					cliente.lojaUltimaCompraValido = true
+				}
+			} else {
+				cliente.lojaUltimaCompraValido = false
 			}
 			clientes = append(clientes, cliente)
 		}
@@ -163,18 +187,18 @@ func insertRows(clientes []Cliente) {
 	fmt.Println(fmt.Sprintf("Clientes Encontrados: %v", len(clientes)))
 	fmt.Println(fmt.Sprintf("Inicio montagem query: %v", time.Now()))
 	// Cria conexão com o banco
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		DB_USER, DB_PASSWORD, DB_NAME)
+	dbinfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
+		DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 	db, err := sql.Open("postgres", dbinfo)
 	checkErr(err)
 	defer db.Close()
 
 	db.Exec("DELETE FROM CLIENTES")
-	query := "INSERT INTO CLIENTES (cpf, private, incompleto, data_ultima_compra, ticket_medio, ticket_ultima_compra, loja_mais_frequent, loja_ultima_compra, created_at) VALUES "
+	query := "INSERT INTO CLIENTES (cpf, cpf_valido, private, incompleto, data_ultima_compra, ticket_medio, ticket_ultima_compra, loja_mais_frequente, loja_mais_frequente_valido, loja_ultima_compra, loja_ultima_compra_valido, created_at) VALUES "
 
 	for idx, cliente := range clientes {
-		row := fmt.Sprintf(" ('%v', %v, %v, '%v', %v, %v, '%v', '%v', current_timestamp) ",
-			cliente.cpf, cliente.private, cliente.incompleto, cliente.dataUltimaCompra.Format("2006-01-02"), cliente.ticketMedio, cliente.ticketUltimaCompra, cliente.lojaMaisFrequente, cliente.lojaUltimaCompra)
+		row := fmt.Sprintf(" ('%v', %v, %v, %v, '%v', %v, %v, '%v', %v, '%v', %v, current_timestamp) ",
+			cliente.cpf, cliente.cpfValido, cliente.private, cliente.incompleto, cliente.dataUltimaCompra.Format("2006-01-02"), cliente.ticketMedio, cliente.ticketUltimaCompra, cliente.lojaMaisFrequente, cliente.lojaMaisFrequenteValido, cliente.lojaUltimaCompra, cliente.lojaUltimaCompraValido)
 		query += row
 		if idx < len(clientes)-1 {
 			query += ","
@@ -187,4 +211,113 @@ func insertRows(clientes []Cliente) {
 
 	fmt.Println(fmt.Sprintf("Fim: %v", time.Now()))
 
+}
+
+func ValidaCPF(cpf string) error {
+	cpf = strings.Replace(cpf, ".", "", -1)
+	cpf = strings.Replace(cpf, "-", "", -1)
+	if len(cpf) != 11 {
+		return errors.New("CPF inválido")
+	}
+	var eq bool
+	var dig string
+	for _, val := range cpf {
+		if len(dig) == 0 {
+			dig = string(val)
+		}
+		if string(val) == dig {
+			eq = true
+			continue
+		}
+		eq = false
+		break
+	}
+	if eq {
+		return errors.New("CPF inválido")
+	}
+	i := 10
+	sum := 0
+	for index := 0; index < len(cpf)-2; index++ {
+		pos, _ := strconv.Atoi(string(cpf[index]))
+		sum += pos * i
+		i--
+	}
+	prod := sum * 10
+	mod := prod % 11
+	if mod == 10 {
+		mod = 0
+	}
+	digit1, _ := strconv.Atoi(string(cpf[9]))
+	if mod != digit1 {
+		return errors.New("CPF inválido")
+	}
+	i = 11
+	sum = 0
+	for index := 0; index < len(cpf)-1; index++ {
+		pos, _ := strconv.Atoi(string(cpf[index]))
+		sum += pos * i
+		i--
+	}
+	prod = sum * 10
+	mod = prod % 11
+	if mod == 10 {
+		mod = 0
+	}
+	digit2, _ := strconv.Atoi(string(cpf[10]))
+	if mod != digit2 {
+		return errors.New("CPF inválido")
+	}
+	return nil
+}
+
+func validaCNPJ(cnpj string) error {
+	cnpj = strings.Replace(cnpj, ".", "", -1)
+	cnpj = strings.Replace(cnpj, "-", "", -1)
+	cnpj = strings.Replace(cnpj, "/", "", -1)
+	if len(cnpj) != 14 {
+		return errors.New("CNPJ inválido")
+	}
+	algs := []int{5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2}
+	var algProdCpfDig1 = make([]int, 12, 12)
+	for key, val := range algs {
+		intParsed, _ := strconv.Atoi(string(cnpj[key]))
+		sumTmp := val * intParsed
+		algProdCpfDig1[key] = sumTmp
+	}
+	sum := 0
+	for _, val := range algProdCpfDig1 {
+		sum += val
+	}
+	digit1 := sum % 11
+	if digit1 < 2 {
+		digit1 = 0
+	} else {
+		digit1 = 11 - digit1
+	}
+	char12, _ := strconv.Atoi(string(cnpj[12]))
+	if char12 != digit1 {
+		return errors.New("CNPJ inválido")
+	}
+	algs = append([]int{6}, algs...)
+	var algProdCpfDig2 = make([]int, 13, 13)
+	for key, val := range algs {
+		intParsed, _ := strconv.Atoi(string(cnpj[key]))
+		sumTmp := val * intParsed
+		algProdCpfDig2[key] = sumTmp
+	}
+	sum = 0
+	for _, val := range algProdCpfDig2 {
+		sum += val
+	}
+	digit2 := sum % 11
+	if digit2 < 2 {
+		digit2 = 0
+	} else {
+		digit2 = 11 - digit2
+	}
+	char13, _ := strconv.Atoi(string(cnpj[13]))
+	if char13 != digit2 {
+		return errors.New("CNPJ inválido")
+	}
+	return nil
 }
